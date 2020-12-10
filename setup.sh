f87bfbda8a3885e3ee50ca11a3f42cb5b36b15e9
arccoxx <arcco96@gmail.com>
2020-12-10 16:23:06 -0500

    setup updated

--- setup.sh
+++ setup.sh
@@ -1,6 +1,11 @@
 
+#!/bin/bash
+
+cd "$(dirname "$0")"
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 sudo python3 get-pip.py 
+pip install pygame
 pip install pygame_gui
 python3 -m pip install -U pygame --user
+python3 game.py
 
